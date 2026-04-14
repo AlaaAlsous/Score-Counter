6685727721b51e595b2060ccb2f2f374129a46d7
Alaa Alsous <lawyeralaa@gmail.com>
2026-04-14 09:18:06 +0200

    Add CORS configuration

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,8 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 var app = builder.Build();
 
 app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
+app.UseCors();
 
 app.MapGet("/api/test", () => "test");
 
