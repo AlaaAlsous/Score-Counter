105193dd7cad0d5a309010685aaaabdb7fa2ea28
Alaa Alsous <lawyeralaa@gmail.com>
2026-05-15 19:43:12 +0200

    Update deployment script (deploy.ps1)

--- deploy.ps1
+++ deploy.ps1
@@ -1,13 +1,30 @@
-$BUILD_TARGET = "linux-x64"
-$BUILD_FOLDER = "./Backend/publish"
-$WEBAPP_NAME = "scorecounter"
-$RESOURCE_GROUP = "RG_ScoreCounter"
+$RESOURCE_GROUP="Score-Counter"
+$APPNAME="Score-Counter-App"
+$NET_VERSION="net10.0"
+$PUBLISH_DIR = ".\bin\Release\$NET_VERSION\publish"
 
-Remove-Item -Force "$env:TEMP\archive.zip" -ErrorAction SilentlyContinue
-Remove-Item -Recurse -Force $BUILD_FOLDER -ErrorAction SilentlyContinue
+$ErrorActionPreference = "Stop"
+Set-StrictMode -Version Latest
 
-dotnet publish ./Backend -c Release -r $BUILD_TARGET -o $BUILD_FOLDER
 
-Compress-Archive -Path "$BUILD_FOLDER/*" -DestinationPath "$env:TEMP\archive.zip"
+Write-Host "🧹 Cleaning..."
+Remove-Item ".\bin\Release" -Recurse -Force -Confirm:$false -ErrorAction SilentlyContinue
 
-az webapp deploy --name $WEBAPP_NAME --resource-group $RESOURCE_GROUP --src-path "$env:TEMP\archive.zip" --type zip
+Write-Host "📦 Publishing..."
+dotnet publish -c Release -o $PUBLISH_DIR
+
+Write-Host "🧹 Cleaning PDB files..."
+Remove-Item "$PUBLISH_DIR\*.pdb" -Force -Confirm:$false -ErrorAction SilentlyContinue
+
+Write-Host "🗜 Creating ZIP..."
+Remove-Item "deploy.zip" -Force -Confirm:$false -ErrorAction SilentlyContinue
+Compress-Archive -Path "$PUBLISH_DIR\*" -DestinationPath "deploy.zip" -Force
+
+Write-Host "☁ Deploying to Azure..."
+az webapp deployment source config-zip `
+  --resource-group $RESOURCE_GROUP `
+  --name $APPNAME `
+  --src ".\deploy.zip"
+
+Remove-Item "deploy.zip" -Force -Confirm:$false -ErrorAction SilentlyContinue
+Write-Host "✅ Done!"
