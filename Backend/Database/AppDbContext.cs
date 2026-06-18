6860d4b62b1387e6668c9c6dab537e7e80cd6198
Alaa Alsous <lawyeralaa@gmail.com>
2026-06-18 21:57:12 +0200

    Fix EF Core ValueComparer for List<string> metadata.

--- Backend/Database/AppDbContext.cs
+++ Backend/Database/AppDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Shared;
 
 namespace Backend.Database;
@@ -19,6 +20,13 @@ public class AppDbContext : DbContext
             .HasConversion(
                 v => string.Join(',', v),
                 v => v.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList()
+            )
+            .Metadata.SetValueComparer(
+                new ValueComparer<List<string>>(
+                    (c1, c2) => (c1 ?? new()).SequenceEqual(c2 ?? new()),
+                    c => c.Aggregate(0, (a, v) => HashCode.Combine(a, v.GetHashCode())),
+                    c => c.ToList()
+                )
             );
     }
 }
